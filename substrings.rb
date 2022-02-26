f72a51fd9527285bc4d6240af18e651891b0500e
Mimmolucio <domenico.lp@gmail.com>
2022-02-26 10:54:47 +0100

    Little refactoring

--- substrings.rb
+++ substrings.rb
@@ -1,21 +1,19 @@
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 
 def substrings(string, dictionary)
-    # Temporary Hash for scan
-    found_words = {}
-    # Final cleaned Hash
-    result = {}
-    
-    dictionary.each do |word|
-        # Scan for words matching
-        found_words[word] = string.downcase.scan(word).count
-        # Only put words with matches > 0 in final Hash
-        result[word] = found_words[word] unless found_words[word] == 0
-    end
+  # Temporary Hash for scan
+  found_words = {}
+  # Final cleaned Hash
+  result = {}
 
-    result
-        
+  dictionary.each do |word|
+    # Scan for words matching
+    found_words[word] = string.downcase.scan(word).count
+    # Only put words with matches > 0 in final Hash
+    result[word] = found_words[word] unless (found_words[word]).zero?
+  end
 
+  result
 end
 
 puts substrings("Howdy partner, sit down! How's it going?", dictionary)
